1d95cea0aea86b32694d47bb554ab3c4e75d9228
River Kim <bluuminn@gmail.com>
2023-04-03 04:14:13 +0900

    test: HelloController 코드를 롬복으로 전환

--- src/test/java/com/bluuminn/spring/webservice/web/HelloControllerTest.java
+++ src/test/java/com/bluuminn/spring/webservice/web/HelloControllerTest.java
@@ -8,6 +8,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.hamcrest.Matchers.is;
+
 
 @WebMvcTest(HelloController.class)
 class HelloControllerTest {
@@ -22,5 +24,21 @@ class HelloControllerTest {
         mvc.perform(MockMvcRequestBuilders.get("/hello"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string(hello));
+    }
+
+    @DisplayName("helloDto가 리턴된다")
+    @Test
+    void return_helloDto() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", is(name)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.amount", is(amount)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(name))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.amount").value(amount));
     }
 }
