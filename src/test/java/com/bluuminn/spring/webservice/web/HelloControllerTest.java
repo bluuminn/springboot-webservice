8ffc9297475a4bd580e241af3e049ea4dce77c7c
River Kim <bluuminn@gmail.com>
2023-04-03 03:54:40 +0900

    test: HelloController 테스트 코드 작성

---
+++ src/test/java/com/bluuminn/spring/webservice/web/HelloControllerTest.java
@@ -0,0 +1,26 @@
+package com.bluuminn.spring.webservice.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @DisplayName("hello가 리턴된다")
+    @Test
+    void return_hello() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(hello));
+    }
+}
