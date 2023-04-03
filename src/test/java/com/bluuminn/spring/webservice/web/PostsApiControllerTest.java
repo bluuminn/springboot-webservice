160fbc2e56db521b12b705e6305837b499f57860
River Kim <bluuminn@gmail.com>
2023-04-03 13:43:53 +0900

    test: 등록 API 테스트

---
+++ src/test/java/com/bluuminn/spring/webservice/web/PostsApiControllerTest.java
@@ -0,0 +1,62 @@
+package com.bluuminn.spring.webservice.web;
+
+import com.bluuminn.spring.webservice.domain.posts.Posts;
+import com.bluuminn.spring.webservice.domain.posts.PostsRepository;
+import com.bluuminn.spring.webservice.web.dto.PostsSaveRequestDto;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PostsApiControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @AfterEach
+    void teardown() {
+        log.info("--- teardown ---");
+        postsRepository.deleteAll();
+    }
+
+    @DisplayName("Posts 등록")
+    @Test
+    void save_posts() {
+        // given
+        String title = "title";
+        String content = "content";
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("author")
+                .build();
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+}
