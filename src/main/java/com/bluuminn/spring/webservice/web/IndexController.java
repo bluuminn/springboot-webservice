672a5a5c47d94bc1971036c3e30c07f86000481b
River Kim <bluuminn@gmail.com>
2023-04-04 17:12:17 +0900

    feat: @LoginUser 사용

--- src/main/java/com/bluuminn/spring/webservice/web/IndexController.java
+++ src/main/java/com/bluuminn/spring/webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.bluuminn.spring.webservice.web;
 
+import com.bluuminn.spring.webservice.config.auth.LoginUser;
 import com.bluuminn.spring.webservice.config.auth.dto.SessionUser;
 import com.bluuminn.spring.webservice.service.PostsService;
 import com.bluuminn.spring.webservice.web.dto.PostsResponseDto;
@@ -18,9 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
