1e1ba8c9d7d0c9b0edb11b64e069be30ccf0b8c7
River Kim <bluuminn@gmail.com>
2023-04-07 17:42:10 +0900

    feat: /profile 인증없이 호출될 수 있도록 SecurityConfig에 추가

---
+++ src/test/java/com/bluuminn/spring/webservice/web/ProfileControllerTest.java
@@ -0,0 +1,32 @@
+package com.bluuminn.spring.webservice.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ProfileControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @DisplayName("인증없이 /profile 호출")
+    @Test
+    void call_profile_with_authentication() throws Exception {
+        String expected = "default";
+
+        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(expected);
+    }
+}
