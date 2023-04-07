576ba254158d5e72128d5f439b2e4a8dbf7d2b72
River Kim <bluuminn@gmail.com>
2023-04-08 00:31:12 +0900

    fix: 무중단 배포 스크립트 수정

--- scripts/health.sh
+++ scripts/health.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-ABSPATH=$(reallink -f $0)
-ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.sh
-source ${ABSDIR}/switch.sh
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname "${ABSPATH}")
+source "${ABSDIR}"/profile.sh
+source "${ABSDIR}"/switch.sh
 
 IDLE_PORT=$(find_idle_port)
 
 echo "> Health Check Start!"
-echo "> IDLE_PORT: $IDLE_PORT"
-echo "> curl -s http://localhost:$IDLE_PORT/profile"
+echo "> IDLE_PORT: ${IDLE_PORT}"
+echo "> curl -s http://localhost:${IDLE_PORT}/profile"
 sleep 10
 
 for RETRY_COUNT in {1..10}; do
-  RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
-  UP_COUNT=$(echo ${RESPONSE} | grep 'prod' | we -1)
+  RESPONSE=$(curl -s http://localhost:"${IDLE_PORT}"/actuator/health)
+  UP_COUNT=$(echo "${RESPONSE}" | grep -c "UP")
 
-  if [ ${UP_COUNT} -ge 1 ]; then # $up_count >= 1 ("prod" 문자열이 있는지 검증)
+  if [ "${UP_COUNT}" -ge 1 ]; then # $up_count >= 1 ("prod" 문자열이 있는지 검증)
     echo "> Health check 성공"
     switch_proxy
     break
@@ -25,11 +25,12 @@ for RETRY_COUNT in {1..10}; do
     echo "> Health check: ${RESPONSE}"
   fi
 
-  if [ ${RETRY_COUNT} -eq 10 ]; then
+  if [ "${RETRY_COUNT}" -eq 10 ]; then
     echo "> Health check 실패"
     echo "> nginx에 연결하지 않고 배포를 종료합니다."
     exit 1
   fi
+
   echo "> Health check 연결 실패. 재시도..."
   sleep 10
 done
--- scripts/profile.sh
+++ scripts/profile.sh
@@ -2,15 +2,15 @@
 
 # 쉬고 있는 profile 찾기: prod1 사용 중 -> prod2 쉬는 중. prod2 사용 중 -> prod1 쉬는 중.
 function find_idle_profile() {
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code} http://localhost/profile")
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-  if [ ${RESPONSE_CODE} -ge 400 ]; then # 400 보다 크면(즉, 40x/50x 에러 모두 포함)
+  if [ "${RESPONSE_CODE}" -ge 400 ]; then # 400 보다 크면(즉, 40x/50x 에러 모두 포함)
     CURRENT_PROFILE=prod2
   else
     CURRENT_PROFILE=$(curl -s http://localhost/profile)
   fi
 
-  if [ ${CURRENT_PROFILE} == prod1 ]; then
+  if [ "${CURRENT_PROFILE}" == prod1 ]; then
     IDLE_PROFILE=prod2
   else
     IDLE_PROFILE=prod1
@@ -21,9 +21,9 @@ function find_idle_profile() {
 
 # 쉬고 있는 profile의 port 찾기
 function find_idle_port() {
-  IDLE_PROFILE=${find_idle_profile}
+  IDLE_PROFILE=$(find_idle_profile)
 
-  if [ ${IDLE_PROFILE} == prod1 ]; then
+  if [ "${IDLE_PROFILE}" == prod1 ]; then
     echo "8081"
   else
     echo "8082"
